f3a69dc8dd9242e7de847afaebb6c96da1bd6cb8
kumar ravi <hi@kumarravi.me>
2019-02-07 07:09:32 +0530

    force https
    Signed-off-by: kumar ravi <hi@kumarravi.me>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment(['production', 'beta'])) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
